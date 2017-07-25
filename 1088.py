575661fffa69c9379df84f56e1212d9b3838055d
czp001 <30425821+czp001@users.noreply.github.com>
2017-07-25 16:07:38 +0800

    Create 1088.py

---
+++ 1088.py
@@ -0,0 +1,20 @@
+s=input()
+def manacher(s):
+    s='#'+'#'.join(s)+'#'
+    RL=[0]*len(s)
+    MaxRight=0
+    pos=0
+    MaxLen=0
+    for i in range(len(s)):
+        if i<MaxRight:
+            RL[i]=min(RL[2*pos-i], MaxRight-i)
+        else:
+            RL[i]=1
+        while i-RL[i]>=0 and i+RL[i]<len(s) and s[i-RL[i]]==s[i+RL[i]]:
+            RL[i]+=1
+        if RL[i]+i-1>MaxRight:
+            MaxRight=RL[i]+i-1
+            pos=i
+        MaxLen=max(MaxLen, RL[i])
+    return MaxLen-1
+print(manacher(s))
