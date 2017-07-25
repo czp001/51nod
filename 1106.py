27a1fd8da9bf5d07ea2eb0e7b9f94b2bf1c8867e
czp001 <30425821+czp001@users.noreply.github.com>
2017-07-25 16:06:34 +0800

    Create 1106.py

---
+++ 1106.py
@@ -0,0 +1,35 @@
+from math import sqrt
+import random
+def miller_rabin(n):
+    d = n - 1
+    s = 0
+    while d % 2 == 0:
+        d >>= 1
+        s += 1
+    for repeat in range(20):
+        a = 0
+        while a == 0:
+            a = random.randrange(n)
+        if not miller_rabin_pass(a, s, d, n):
+            return False
+    return True
+
+def miller_rabin_pass(a, s, d, n):
+    a_to_power = pow(a, d, n)
+    if a_to_power == 1:
+        return True
+    for i in range(s-1):
+        if a_to_power == n - 1:
+            return True
+        a_to_power = (a_to_power * a_to_power) % n
+    return a_to_power == n - 1
+
+n=input()
+s=[]
+for i in range(n):
+    a=input()
+    s.append(a)
+
+for i in s:
+    if miller_rabin(i):print 'Yes'
+    else:print 'No'
